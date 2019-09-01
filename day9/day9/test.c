dba5ae3dd484204728e36c72685c8ed0e97e16f2
田佳 <1365751272@qq.com>
2019-09-01 23:22:39 +0800

    子数组和最大值

--- day9/day9/test.c
+++ day9/day9/test.c
@@ -1,98 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
-int Partition(int arr[], int left, int right)
+#include<assert.h>
+int MAX_Arry(int *arr, int len)
 {
-	int tmp = arr[left];
-	while (left < right)
-	{
-		//右边 找比tmp小的数据
-		while (left < right && arr[right] >= tmp)
-		{
-			right--;
-		}
-		if (left >= right)
-		{
-			//arr[left] = tmp;
-			break;
-		}
-		else
-		{
-			arr[left] = arr[right];
-		}
-		//左边的   比tmp大的数据
-		while (left < right && arr[left] <= tmp)
-		{
-			left++;
-		}
-		if (left >= right)
-		{
-			//arr[left] = tmp;
-			break;
-		}
-		else
-		{
-			arr[right] = arr[left];
-		}
-	}
-	arr[left] = tmp;
-	return left;
-}
-int BeyondArrayLengthHalf(int *arr, int len)
-{
-	int mid = len / 2;
-	int left = 0;
-	int right = len - 1;
-	int par = Partition(arr, left, right);
-	int result = 0;
-	if (arr == NULL || len < 0)
-	{
-		return 0;//假设数组当中不存放数字0
-	}
-	while (par != mid)
-	{
-		if (par > mid)
-		{
-			right = par - 1;
-			par = Partition(arr, left, right);
-		}
-		if (par < mid)
-		{
-			left = par + 1;
-			par = Partition(arr, left, right);
-		}
-	}
-	result = arr[mid];
-	if (!CheakMoreThanHalf(arr, len, result))
+	int Max = arr[0];
+	int sum = arr[0];
+	int i = 0;
+	if (arr == NULL || len <= 1)
 	{
 		return 0;
 	}
-	return result;
-}
-int CheakMoreThanHalf(int *arr, int len, int val)
-{
-	int count = 0;
-	int i = 0;
 	for (i = 0; i < len; i++)
 	{
-		if (arr[i] == val)
+		if (sum < 0)
 		{
-			count++;
+			sum = arr[i];
+		}
+		else
+			sum += arr[i];
+		if (sum>Max)
+		{
+			Max = sum;
 		}
 	}
-	if (count >= len / 2)
-	{
-		return 1;
-	}
-	return 0;
-
+	return Max;
 }
 int main()
 {
-	int ret = 0;
-	int arr[] = {1,2,3,2,2,2,0,2 };
+	int arr[] = { 1, -2, 3, 10, -4, 7, 2, -5 };
 	int len = sizeof(arr) / sizeof(arr[0]);
-	ret = BeyondArrayLengthHalf(arr, len);
-	printf("%d\n", ret);
+	printf("%d\n",  MAX_Arry(arr, len));
 	system("pause");
 	return 0;
 }
