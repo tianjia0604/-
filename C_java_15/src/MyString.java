fd2f678f0bfdb753381f5e67241e0175fee413cb
田佳 <1365751272@qq.com>
2019-11-27 23:37:09 +0800

    字符串基本操作

---
+++ C_java_15/src/MyString.java
@@ -0,0 +1,51 @@
+public class MyString {
+    public static void main(String[] args) {
+//        String str1 = "hello world";
+//        String str2 = "Hello World";
+//        System.out.println(str1.equals(str2));
+//        System.out.println(str1.equalsIgnoreCase(str2));
+//
+//        System.out.println("A".compareTo("a"));
+//        System.out.println("a".compareTo("a"));
+//        System.out.println("a".compareTo("A"));
+
+//        String str1 = "hello world";
+//        String str2 = "world";
+//        System.out.println(str1.contains(str2));//true
+//
+//        String str = "helloworld";
+
+//        System.out.println(str.substring(3));//loworld
+//        System.out.println(str.substring(2,5));//llo
+//        System.out.println(str.replaceAll("l","_"));//he__owor_d
+//        System.out.println(str.replaceFirst("l","_"));//he_loworld
+//        System.out.println(str.indexOf("w"));//5
+//        System.out.println(str.indexOf("low"));//3
+//        System.out.println(str.indexOf("l",5));//8
+//        System.out.println(str.lastIndexOf("w"));//5
+//        System.out.println(str.lastIndexOf("l",4));//3
+//
+//        String str = "we are happy !";
+//        String[] result = str.split(" ",3);//按照空格拆分
+//        for (String i : result) {
+//            System.out.println(i);
+//        }
+//        String str = "name=zhangsan&age=18" ;
+//        String[] result = str.split("&") ;
+//        for (int i = 0; i < result.length; i++) {
+//            String[] tmp = result[i].split("=") ;
+//            System.out.println(tmp[0]+" = "+tmp[1]);
+        String str = "hello world";
+        char[] c = str.toCharArray();
+        System.out.println(c);
+//        System.out.println(str.length());//11
+//        System.out.println("["+str+"]");
+//        System.out.println("["+str.trim()+"]" );
+
+
+//        System.out.println("hello".isEmpty());
+//        System.out.println("".isEmpty());
+//        System.out.println(new String().isEmpty());
+    }
+}
+
