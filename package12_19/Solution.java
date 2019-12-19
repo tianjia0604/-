f7692a10a9ad228ade9f01fa91de14c6c383acc1
田佳 <1365751272@qq.com>
2019-12-19 23:50:05 +0800

    连续子数组的最大和

---
+++ package12_19/Solution.java
@@ -0,0 +1,14 @@
+package package12_19;
+public class Solution {
+    public int FindGreatestSumOfSubArray(int[] array) {
+        if(array == null)
+            return 0;
+        int sum = array[0];
+        int tmpSum = array[0];
+        for(int i = 1;i < array.length;i++) {
+            tmpSum = (tmpSum < 0) ? array[i] : tmpSum + array[i];
+            sum = (tmpSum > sum) ? tmpSum : sum;
+        }
+        return sum;
+    }
+}
