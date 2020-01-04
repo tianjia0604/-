0cd625a9fdaacfd16f38c41f1d96d828981deb9e
田佳 <1365751272@qq.com>
2020-01-04 14:54:31 +0800

    滑动窗口的最大值

---
+++ package1_4/Solution.java
@@ -0,0 +1,30 @@
+package package1_4;
+
+import java.util.*;
+public class Solution {
+    public ArrayList<Integer> maxInWindows(int [] num, int size)
+    {
+        if (num == null || num.length == 0 || size <= 0 || num.length < size) {
+            return new ArrayList<Integer>();
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        //双端队列，用来记录每个窗口的最大值下标
+        LinkedList<Integer> qmax = new LinkedList<>();
+        int index = 0;
+        for (int i = 0; i < num.length; i++) {
+            while (!qmax.isEmpty() && num[qmax.peekLast()] < num[i]) {
+                qmax.pollLast();
+            }
+            qmax.addLast(i);
+            //判断队首元素是否过期
+            if (qmax.peekFirst() == i - size) {
+                qmax.pollFirst();
+            }
+            //向result列表中加入元素
+            if (i >= size - 1) {
+                result.add(num[qmax.peekFirst()]);
+            }
+        }
+        return result;
+    }
+}
