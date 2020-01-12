b04b2cdc9d7a232672c17f986e45ff8230438ba1
田佳 <1365751272@qq.com>
2020-01-12 23:10:29 +0800

    数据流中的中位数

---
+++ package1_12/Solution.java
@@ -0,0 +1,44 @@
+package package1_12;
+
+import java.util.PriorityQueue;
+import java.util.Comparator;
+public class Solution {
+
+    //小顶堆
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    //大顶堆
+    private PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(15, new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1;
+        }
+    });
+
+    //记录偶数个还是奇数个
+    int count = 0;
+    //每次插入小顶堆的是当前大顶堆中最大的数
+    //每次插入大顶堆的是当前小顶堆中最小的数
+    //这样保证小顶堆中的数永远大于等于大顶堆中的数
+    //中位数就可以方便地从两者的根结点中获取了
+    public void Insert(Integer num) {
+        //个数为偶数的话，则先插入到大顶堆，然后将大顶堆中最大的数插入小顶堆中
+        if(count % 2 == 0){
+            maxHeap.offer(num);
+            int max = maxHeap.poll();
+            minHeap.offer(max);
+        }else{
+            //个数为奇数的话，则先插入到小顶堆，然后将小顶堆中最小的数插入大顶堆中
+            minHeap.offer(num);
+            int min = minHeap.poll();
+            maxHeap.offer(min);
+        }
+        count++;
+    }
+    public Double GetMedian() {
+        if(count % 2 == 0) {
+            return new Double(minHeap.peek() + maxHeap.peek()) / 2;
+        }else {
+            return new Double(minHeap.peek());
+        }
+    }
+}
