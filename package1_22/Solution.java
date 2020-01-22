5b5f6db49fa9bb7fa526106f03f1c9803501afed
田佳 <1365751272@qq.com>
2020-01-22 22:51:44 +0800

    两数之和/二进制求和

---
+++ package1_22/Solution.java
@@ -0,0 +1,38 @@
+package package1_22;
+public class Solution {
+    //一、两数之和
+    public int[] twoSum(int[] nums, int target) {
+        int[] index = new int[2];
+        for(int i = 0;i < nums.length;i++) {
+            for(int j = nums.length  - 1;j > i;j--) {
+                if(nums[i]+nums[j] == target) {
+                    index[0] = i;
+                    index[1] = j;
+                    return index;
+                }
+            }
+        }
+        return index;
+    }
+    //二、二进制求和
+    public String addBinary(String a, String b) {
+        if(a == null || a.length() == 0) return b;
+        if(b == null || b.length() == 0) return a;
+
+        StringBuilder stb = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        int c = 0;  // 进位
+        while(i >= 0 || j >= 0) {
+            if(i >= 0) c += a.charAt(i --) - '0';
+            if(j >= 0) c += b.charAt(j --) - '0';
+            stb.append(c % 2);
+            c >>= 1;
+        }
+
+        String res = stb.reverse().toString();
+        return c > 0 ? '1' + res : res;
+    }
+
+}
