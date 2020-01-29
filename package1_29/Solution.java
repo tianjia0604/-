cfc2ae3359acfa933d32518235d8b782a7719eb3
田佳 <1365751272@qq.com>
2020-01-29 23:02:32 +0800

    压缩字符串

---
+++ package1_29/Solution.java
@@ -0,0 +1,24 @@
+package package1_29;
+
+public class Solution {
+    public int compress(char[] chars) {
+        int left = 0;
+        int size = 0;
+        // 由于最后一个字符也需要判断，所以将右指针终点放到数组之外一格
+        for (int right = 0; right <= chars.length; right++) {
+            // 当遍历完成，或右指针元素不等于左指针元素时，更新数组
+            if (right == chars.length || chars[right] != chars[left]) {
+                chars[size++] = chars[left];    // 更新字符
+                if (right - left > 1) {         // 更新计数，当个数大于 1 时才更新
+                    for(char c : String.valueOf(right - left).toCharArray()) {
+                        chars[size++] = c;
+                    }
+                }
+                left = right;
+            }
+        }
+
+        return size;
+    }
+
+}
